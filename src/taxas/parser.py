83d835badb9cc5a09dfe431226e1a86a786b4a36
FlaviuVadan <flaviuvadan@gmail.com>
2019-10-29 15:57:00 -0600

    Implement create_gene_freq_plot

--- src/taxas/parser.py
+++ src/taxas/parser.py
@@ -1,6 +1,8 @@
 import csv
 import os
 
+import matplotlib.pyplot as plt
+
 
 class Parser:
     """ The parser is responsible for parsing the organism gene frequencies and the gene collections for plotting """
@@ -11,6 +13,7 @@ class Parser:
     def __init__(self, genes_file_path):
         """ Constructor """
         self.genes = self.load_genes(genes_file_path)
+        self.gene_freqs = self._parse_gene_freqs()
 
     def load_genes(self, genes_file_path):
         """ Reads in the genes """
@@ -19,21 +22,34 @@ class Parser:
             for gene in csv_reader:
                 yield (gene[self.GENE_NAME_IDX], gene[self.GENE_ID_IDX])
 
-    def parse(self):
-        """ Parses the two files - class doc """
-        pass
-
     def _parse_gene_freqs(self):
         """ Parses the gene frequencies file """
-        pass
+        frequencies = {}
+        with open("gene_frequencies.txt", "r") as freqs:
+            freqs.readline()  # omit the first line
+            for line in freqs.readlines():
+                split_line = line.split(",")
+                org_name, gene_freq = split_line[0], int(split_line[1].replace("\n", ""))
+                frequencies[org_name] = gene_freq
+        return frequencies
+
+    def create_gene_freq_plot(self):
+        """ Creates a plot of the gene frequencies """
+        plt.figure(figsize=(18, 18))
+        plt.scatter(self.gene_freqs.keys(), self.gene_freqs.values(), marker='.')
+        plt.title("Organisms gene frequencies")
+        plt.axis([0, len(self.gene_freqs.keys()), 20, max(self.gene_freqs.values()) + 3])
+        plt.xticks(list(self.gene_freqs.keys()), rotation='vertical')
+        plt.tick_params(axis='x', which='major', labelsize=5)
+        plt.savefig("gene_freqs.pdf", format="pdf", quality=95)
+        plt.close()
 
     def _parse_organisms_genes(self):
         """ Parses the organisms genes file """
         pass
 
-    def
-
 
 if __name__ == "__main__":
     gene_file_path = os.path.join(os.pardir, "data", "genes.txt")
     parser = Parser(gene_file_path)
+    parser.create_gene_freq_plot()
