bb9d83e2f31dfb5e419ffb92979d4c3eb28987ab
FlaviuVadan <flaviuvadan@gmail.com>
2019-10-31 20:46:34 -0600

    Start building the MSA parser

--- src/alignments/parser.py
+++ src/alignments/parser.py
@@ -1,17 +1,58 @@
 import os
 
-base_path = "/Users/FlaviuVadan/Documents/flav/current_courses/BINF400/proteoglycan-pathway-evolution/src/data"
-gene_path = os.path.join(base_path, "organisms/Acan_ENSG00000157766.txt")
-organisms = []
-with open(gene_path, 'r') as f:
-    for l in f.readlines():
-        if l.startswith(">"):
-            organisms.append(l.replace(">", "").strip())
 
-for org in organisms:
-    os.system("bash parser.sh {}".format(org))
-    break
+class Parser:
+    """ Parser is responsible for parsing the multiple sequence alignment files from data/alignments """
 
-with open(os.path.join(base_path, "alignments/Acan_msa.temp"), "r") as f:
-    alignment = f.read()
-    print(alignment.replace("homo_sapiens", "").strip().replace(" ", "").replace("\n", ""))
+    ORGANISM_IDX = 0
+    ALIGNMENT_IDX = 1
+
+    def __init__(self, base_path):
+        """ Constructor """
+        self.base_path = base_path
+        self.alignment_file_paths = self._build_alignment_paths()
+        self.alignments = self._parse_alignments()
+        self.frequencies = self._build_frequencies()
+
+    def _get_alignment_filenames(self):
+        """ Returns a list of all the MSA filenames """
+        return os.listdir(self.base_path)
+
+    def _build_alignment_paths(self):
+        """ Returns a list of all the alignments full file paths """
+        alignment_file_names = self._get_alignment_filenames()
+        return [os.path.join(self.base_path, afn) for afn in alignment_file_names]
+
+    def _parse_alignments(self):
+        """ Parses the MSAs to clean up the 60 character break added by ClustalW """
+        # final result structure should be:
+        #   alignments = {filename: {alignments}}
+        final = {}
+        for afp in self.alignment_file_paths:
+            filename = afp.split("/")[-1]  # the filename.txt is sufficient
+            if not final.get(filename):
+                final[filename] = {}
+            with open(afp, "r") as f:
+                for line in f.readlines():
+                    split_line = line.split()
+                    if not split_line:  # this can happen because we have line breaks between alignment blocks
+                        continue
+                    org, alignment = split_line[self.ORGANISM_IDX], split_line[self.ALIGNMENT_IDX]
+                    if not final.get(filename).get(org):
+                        final[filename][org] = ""
+                    final[filename][org] = final.get(filename).get(org) + alignment
+        return final
+
+    def _build_frequencies(self):
+        """ Builds a list of lists containing tuples indicating the number of characters per alignment column. This is
+        used for building a simple visualization of the generated MSAs """
+        freqs = {}
+        for gene in self.alignments.keys():
+            if not freqs.get(gene):
+                freqs[gene] = {}
+            gene_alignment = self.alignments.get(gene)
+
+
+
+if __name__ == "__main__":
+    parser = Parser(os.getcwd())
