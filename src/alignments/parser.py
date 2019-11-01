362b2fe3f0c5c7137abed603a6fc510d92829f49
FlaviuVadan <flaviuvadan@gmail.com>
2019-11-01 15:42:07 -0600

    Implement MSA parser and generate MSA plot

--- src/alignments/parser.py
+++ src/alignments/parser.py
@@ -1,5 +1,10 @@
 import os
 
+from matplotlib import collections as matcoll
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn-white')
+
 
 class Parser:
     """ Parser is responsible for parsing the multiple sequence alignment files from data/alignments """
@@ -25,22 +30,23 @@ class Parser:
 
     def _parse_alignments(self):
         """ Parses the MSAs to clean up the 60 character break added by ClustalW """
-        # final result structure should be:
-        #   alignments = {filename: {alignments}}
         final = {}
         for afp in self.alignment_file_paths:
             filename = afp.split("/")[-1]  # the filename.txt is sufficient
             if not final.get(filename):
                 final[filename] = {}
             with open(afp, "r") as f:
+                org = ""  # helps keep track of the last organism
                 for line in f.readlines():
-                    split_line = line.split()
-                    if not split_line:  # this can happen because we have line breaks between alignment blocks
-                        continue
-                    org, alignment = split_line[self.ORGANISM_IDX], split_line[self.ALIGNMENT_IDX]
-                    if not final.get(filename).get(org):
-                        final[filename][org] = ""
-                    final[filename][org] = final.get(filename).get(org) + alignment
+                    if not line.strip():
+                        continue  # it can happen that we have empty lines
+                    line = line.strip("\n")
+                    if ">" in line:
+                        org = line.strip(">")
+                        if not final.get(filename).get(org):
+                            final[filename][org] = ""
+                    else:
+                        final[filename][org] = final.get(filename).get(org) + line
         return final
 
     def _build_frequencies(self):
@@ -49,10 +55,41 @@ class Parser:
         freqs = {}
         for gene in self.alignments.keys():
             if not freqs.get(gene):
-                freqs[gene] = {}
-            gene_alignment = self.alignments.get(gene)
+                freqs[gene] = []
+            for i in range(len(list(self.alignments.get(gene).values())[0])):
+                freq_i = 0
+                for seq in self.alignments.get(gene).values():
+                    try:
+                        if seq[i] != "-":
+                            freq_i = freq_i + 1
+                    except Exception as e:
+                        pass
+                freqs[gene].append((i, freq_i))
+        return freqs
 
+    def build_frequency_plots(self):
+        """ Builds the frequency plots associated with the MSAs """
+        num_plots = len(list(self.frequencies.keys()))
+        fig = plt.figure(figsize=(10, 50))
+        fig.subplots_adjust(hspace=1)
+        for idx, gene in enumerate(list(self.frequencies.keys())):
+            values = self.frequencies.get(gene)
+            x_vals = [x[0] for x in values]
+            y_vals = [x[1] for x in values]
+            lines = []
+            for i in range(len(x_vals)):
+                pair = [(x_vals[i], 0), (x_vals[i], y_vals[i])]
+                lines.append(pair)
+            linecoll = matcoll.LineCollection(lines)
+            ax = fig.add_subplot(num_plots, 1, idx + 1)
+            ax.add_collection(linecoll)
+            plt.scatter(x_vals, y_vals, marker="None")
+            plt.yticks([])
+            title = "{} - MSA".format(gene.split("_")[0])
+            plt.title(title)
+        plt.savefig("MSAs.pdf", format="pdf", quality=95)
 
 
 if __name__ == "__main__":
     parser = Parser(os.getcwd())
+    parser.build_frequency_plots()
